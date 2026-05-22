a2bef2d5fdadb024b770fd5a314480b9815efaa2
tgg112 <edwardcjonkman@gmail.com>
2026-05-22 14:57:31 +1000

    Add files via upload

---
+++ FORKBOMB Visualiser.py
@@ -0,0 +1,10 @@
+import time
+import os
+
+instances = 1
+os.system("title malware visualiser")
+input("this shows how fast the henri virus increase in size, and is not harmful, continue? ")
+
+while True:
+    print(f"There are {instances} tabs open")
+    instances *= 2  # Double the number of instances
